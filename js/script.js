a989b7b50fab06ae43c258f2a2a575a3c142ed4a
EmanueleCozzolino18 <emanuele.cozzolino18@libero.it>
2024-05-08 16:27:47 +0200

    [UPD] We also check if they are multiples of only 3 and only of 5

--- js/script.js
+++ js/script.js
@@ -11,6 +11,16 @@ for (let i = 1; i <= 100; i++) {
 
         console.log('FizzBuzz');
 
-    } else console.log(i) //ALTRIMENTI STAMPA I NUMERI CHE NON SONO ME MULTIPLI DI 3 E 5
+
+    } else if ((i % 5) === 0) { //ANDIAMO A VERIFICARE SE I NUMERI SONO MULTIPLI DI 5
+
+        console.log('Buzz');
+
+    } else if ((i % 3) === 0) { //ANDIAMO A VERIFICARE SE I NUMERI SONO MULTIPLI DI 3
+
+        console.log('Fizz');
+
+    }
+    else console.log(i) //ALTRIMENTI STAMPA I NUMERI CHE NON SONO ME MULTIPLI DI 3 E 5
 }
 
