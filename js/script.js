5db086f9009f0d00e55df7f59662e7f600939280
EmanueleCozzolino18 <emanuele.cozzolino18@libero.it>
2024-05-08 16:24:45 +0200

    [UPD] First condition we only print multiples of 3 and 5

--- js/script.js
+++ js/script.js
@@ -6,5 +6,11 @@ console.log("Lo script funziona correttamente")
 //CICLO FOR CHE CI PERMETTE DI CONTARE I NUMER STAMPATI DA 1 A 100
 for (let i = 1; i <= 100; i++) {
 
+    //PRIMO CONTROLLO ANDIAMO A VERIFICARE SE I NUMERI STAMPATI SONO MULTIPLI DI 3 E 5
+    if ((i % 3) === 0 && (i % 5) === 0) {
+
+        console.log('FizzBuzz');
+
+    } else console.log(i) //ALTRIMENTI STAMPA I NUMERI CHE NON SONO ME MULTIPLI DI 3 E 5
 }
 
